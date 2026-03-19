7036debf9a3759fd706e93c5035c3bbbf31363ff
Darleanow <Darleanow@users.noreply.github.com>
2026-03-19 19:14:57 +0000

    deploy: 589c7920420bd34c832678bb6697b85354d27433

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['check_0',['check',['../classCheckbox.html#a65c8a93773d759de1fc01963d48cf62f',1,'Checkbox']]],
   ['checkbox_1',['Checkbox',['../classCheckbox.html#a123418b89db9d4e50023c337c1ce5104',1,'Checkbox']]],
-  ['checkboxgroup_2',['CheckboxGroup',['../classCheckboxGroup.html#a0122183399e042659ec6709f71c85ecc',1,'CheckboxGroup']]],
-  ['clear_5fscreen_3',['clear_screen',['../namespaceUtils.html#a9c89c809dec2e507c28f654c33ee38a9',1,'Utils']]]
+  ['checkboxgroup_2',['CheckboxGroup',['../classCheckboxGroup.html#a0122183399e042659ec6709f71c85ecc',1,'CheckboxGroup']]]
 ];
