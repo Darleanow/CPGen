cd92c7e304413e0238de8d59bdb1067938b4d766
Darleanow <Darleanow@users.noreply.github.com>
2026-04-05 20:06:02 +0000

    deploy: 3972c58a42b123e413ce12934563fd07401699f6

--- search/functions_9.js
+++ search/functions_9.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['parse_0',['parse',['../classCLI.html#a4b1155e1785fc2b872f54d6878361a1a',1,'CLI']]],
-  ['parse_5fopts_1',['parse_opts',['../classCLI.html#a07b1961f218ddeb09d1eb99d4cdb8f16',1,'CLI']]],
-  ['preferencesmanager_2',['PreferencesManager',['../classPreferencesManager.html#a30c7fd28d19688d125596666fd2132fe',1,'PreferencesManager']]],
-  ['projectresolver_3',['ProjectResolver',['../classProjectResolver.html#a7048b357a6e148ee7260e99d44894b4c',1,'ProjectResolver']]]
+  ['preferencesmanager_1',['PreferencesManager',['../classPreferencesManager.html#a30c7fd28d19688d125596666fd2132fe',1,'PreferencesManager']]],
+  ['projectresolver_2',['ProjectResolver',['../classProjectResolver.html#a7048b357a6e148ee7260e99d44894b4c',1,'ProjectResolver']]]
 ];
